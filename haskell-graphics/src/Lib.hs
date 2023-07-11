87a6799661dc599b4a98efe193deef620a7ab889
Raunak <60561127+RnkSngh@users.noreply.github.com>
2023-07-11 16:50:08 -0400

    clean up lib.hs

--- haskell-graphics/src/Lib.hs
+++ haskell-graphics/src/Lib.hs
@@ -15,42 +15,33 @@ winHeight = 1080
 winTitle = "Serpenski Triangles"
 
 
-callback :: GLFW.KeyCallback
-callback window key scanCode keyState modKeys = do
-    print key
-    when (key == GLFW.Key'Escape && keyState == GLFW.KeyState'Pressed)
-        (GLFW.setWindowShouldClose window True)
-
-
 draw :: IO ()
 draw = do
-    GLFW.init
+    succeeded  <- GLFW.init
     GLFW.defaultWindowHints
     maybeWindow <- GLFW.createWindow winWidth winHeight winTitle Nothing Nothing
     case maybeWindow of
         Nothing -> putStrLn "Failed to create a GLFW window!"
         Just window -> do
-            -- enable keys
-            GLFW.setKeyCallback window (Just callback)
             -- calibrate the viewport
             GLFW.makeContextCurrent (Just window)
             (x,y) <- GLFW.getFramebufferSize window
             glViewport 0 0 (fromIntegral x) (fromIntegral y)
+            glClearColor 0 1.0 0.0 1.0
+            glClear GL_COLOR_BUFFER_BIT
+
+            GLFW.swapBuffers window
             -- enter our main loop
             let loop = do
                     shouldContinue <- not <$> GLFW.windowShouldClose window
                     when shouldContinue $ do
-                        -- event poll
                         GLFW.pollEvents
+                        -- event poll
                         -- drawing
-                        glClearColor 0 1.0 0.0 1.0
-                        glClear GL_COLOR_BUFFER_BIT
                         -- swap buffers and go again
-                        GLFW.swapBuffers window
                         loop
             loop
     GLFW.terminate
 
 someFunc :: IO ()
 someFunc = draw 
-
