922a6089eb1fc00f6d4049f919daa9063ee470aa
Raunak <60561127+RnkSngh@users.noreply.github.com>
2023-09-14 14:49:26 -0400

    :D :D :D :D

--- haskell-graphics/src/Interactivity.hs
+++ haskell-graphics/src/Interactivity.hs
@@ -6,55 +6,24 @@ import Control.Lens
 import Foreign.C.String (newCAStringLen, newCString)
 
 moveUp step pointer = do
-    a <- peek pointer 
-    let x = a ^._x
-    let y = a ^._y
-    let z = a ^._z
-    let w = a ^._w
-    let newX = V4 (x ^._x) (x ^._y) (x ^._z)  (x ^._w + step)
-    let newY = V4 (y ^._x) (y ^._y) (y ^._z)  (y ^._w )
-    let newZ = V4 (z ^._x) (z ^._y) (z ^._z)  (z ^._w )
-    let newA = V4 newX newY newZ w
-    poke pointer (newA)
-    return ()
+    a<- peek pointer 
+    poke pointer (over (_w._y) (\y -> y + step) a)
 
 moveDown step pointer = do
     a <- peek pointer 
-    let x = a ^._x
-    let y = a ^._y
-    let z = a ^._z
-    let w = a ^._w
-    let newX = V4 (x ^._x) (x ^._y) (x ^._z)  (x ^._w)
-    let newY = V4 (y ^._x) (y ^._y) (y ^._z)  (y ^._w - step)
-    let newZ = V4 (z ^._x) (z ^._y) (z ^._z)  (z ^._w )
-    let newA = V4 newX newY newZ w
-    poke pointer (newA)
-    return ()
+    poke pointer (over  (_w._y) (\y -> y - step) a)
+
 
 moveLeft step pointer = do
     a <- peek pointer 
-    let x = a ^._x
-    let y = a ^._y
-    let z = a ^._z
-    let w = a ^._w
-    let newX = V4 (x ^._x) (x ^._y) (x ^._z)  (x ^._w - step)
-    let newY = V4 (y ^._x) (y ^._y) (y ^._z)  (y ^._w )
-    let newZ = V4 (z ^._x) (z ^._y) (z ^._z)  (z ^._w )
-    let newA = V4 newX newY newZ w
-    poke pointer (newA)
-    return ()
+    poke pointer (over (_w._x) (\x -> x + step) a)
 
 moveRight step pointer = do
     a <- peek pointer 
-    let x = a ^._x
-    let y = a ^._y
-    let z = a ^._z
-    let w = a ^._w
-    let newX = V4 (x ^._x) (x ^._y) (x ^._z)  (x ^._w )
-    let newY = V4 (y ^._x) (y ^._y) (y ^._z)  (y ^._w  + step )
-    let newZ = V4 (z ^._x) (z ^._y) (z ^._z)  (z ^._w )
-    let newA = V4 newX newY newZ w
-    poke pointer (newA)
-    return ()
+    poke pointer (over (_w._x) (\x -> x - step) a)
+    
+
+
+
     
 
